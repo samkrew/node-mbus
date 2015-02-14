8f783664238831967b2d1e6e0d4388e5d5f34564
Sam Krew <samkrew@yahoo.com>
2015-02-14 23:00:10 +0200

    Test callback

--- binding.gyp
+++ binding.gyp
@@ -14,6 +14,7 @@
 			],
 			'include_dirs': [
 				'../libmodbus/mbus',
+				"<!(node -e \"require('nan')\")"
 			],
 			'sources': [
 				'./src/main.cc'
--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,20 @@
-main.cc
+#include <nan.h>
+
+using namespace v8;
+
+NAN_METHOD(RunCallback) {
+  NanScope();
+
+  Local<Function> cb = args[0].As<Function>();
+  const unsigned argc = 1;
+  Local<Value> argv[argc] = { NanNew("hello world") };
+  NanMakeCallback(NanGetCurrentContext()->Global(), cb, argc, argv);
+
+  NanReturnUndefined();
+}
+
+void Init(Handle<Object> exports, Handle<Object> module) {
+  NODE_SET_METHOD(module, "exports", RunCallback);
+}
+
+NODE_MODULE(addon, Init)
